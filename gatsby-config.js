7281353118d45e5282d3d2fb1fb4c93e94ab5c9f
Muhammad Ibragimov <mibragimov@hotmail.com>
2020-09-12 09:39:41 +0500

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,28 @@ module.exports = {
     lang: `en`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-177831121-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+      },
+    },
     `gatsby-plugin-react-helmet`,
 
     `gatsby-transformer-sharp`,
@@ -32,6 +54,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
